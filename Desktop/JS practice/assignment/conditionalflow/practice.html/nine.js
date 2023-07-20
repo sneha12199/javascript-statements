87121b1c74d015863307bee6edd73c68ed0083d4
sneha12199 <khoklesneha5@gmail.com>
2023-07-20 09:55:52 +0530

    ok

---
+++ Desktop/JS practice/assignment/conditionalflow/practice.html/nine.js
@@ -0,0 +1,19 @@
+//var cars=[];//declaration of arry
+//cars=["car1","car2","car3","car4","car5"];
+//console.log(cars[4]);
+
+
+
+
+
+//looping statement
+//for (var i=0;i<=500;i++){
+  //  console.log(i);  
+//}for loop i++
+
+
+
+//decrement st=
+//for (var i=20;i>=0;i--){
+  //    console.log(i)
+//}
